129b9928c93d5951450f9551c920d5fcf21cf6e6
leokay1515 <36515738+leokay1515@users.noreply.github.com>
2018-03-27 14:03:42 -0400

    Initial commit

---
+++ PerfectSquaresLeoK/PerfectSquaresLeoKForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PerfectSquaresLeoK
+{
+    partial class frmPerfectSquaresLeoK
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.nudEndValue = new System.Windows.Forms.NumericUpDown();
+            this.lstPerfectSquares = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudEndValue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(93, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(86, 28);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // nudEndValue
+            // 
+            this.nudEndValue.Location = new System.Drawing.Point(82, 46);
+            this.nudEndValue.Name = "nudEndValue";
+            this.nudEndValue.Size = new System.Drawing.Size(111, 20);
+            this.nudEndValue.TabIndex = 1;
+            // 
+            // lstPerfectSquares
+            // 
+            this.lstPerfectSquares.FormattingEnabled = true;
+            this.lstPerfectSquares.Location = new System.Drawing.Point(63, 72);
+            this.lstPerfectSquares.Name = "lstPerfectSquares";
+            this.lstPerfectSquares.Size = new System.Drawing.Size(151, 134);
+            this.lstPerfectSquares.TabIndex = 2;
+            // 
+            // frmPerfectSquaresLeoK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lstPerfectSquares);
+            this.Controls.Add(this.nudEndValue);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmPerfectSquaresLeoK";
+            this.Text = "Perfect Squares by Leo K";
+            ((System.ComponentModel.ISupportInitialize)(this.nudEndValue)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NumericUpDown nudEndValue;
+        private System.Windows.Forms.ListBox lstPerfectSquares;
+    }
+}
+
---
+++ PerfectSquaresLeoK/PerfectSquaresLeoKForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PerfectSquaresLeoK
+{
+    public partial class frmPerfectSquaresLeoK : Form
+    {
+        public frmPerfectSquaresLeoK()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //local variables
+            int endingValue;
+            int value;
+            Double squareRootAsDouble;
+            int squareRootAsInt;
+
+            //Set initial values
+            value = 1;
+
+            //clear the list items
+            this.lstPerfectSquares.Items.Clear();
+
+            //Get the number from the up down box
+            endingValue = Convert.ToInt32(this.nudEndValue.Value);
+
+            //State any perfect squares until you've reached the end value
+            while (value <= endingValue)
+            {
+                //square the value
+                squareRootAsDouble = Math.Sqrt(value);
+
+                //convert the double to an int
+                squareRootAsInt = Convert.ToInt32(squareRootAsDouble);
+
+                //If the int and the double are equal, then its a perfect square
+                if (squareRootAsDouble == squareRootAsInt)
+                {
+                    //add it to the list
+                    this.lstPerfectSquares.Items.Add(value + " is a perfect square");
+                    this.Refresh();
+                }
+                value++;
+            }
+        }
+    }
+}
